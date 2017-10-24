f468ca15fe44b3917710bcef2e150083e9eec910
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-10-24 11:22:57 +0200

    Add more initialization options

--- chapter_two/src/bin/vector.rs
+++ chapter_two/src/bin/vector.rs
@@ -3,12 +3,19 @@ fn main() {
     let fruits = vec!["apple", "tomato", "pear"];
     // A vector cannot be directly printed
     // But we can debug-print it
-    println!("Fruits: {:?}", fruits);
+    println!("fruits: {:?}", fruits);
 
     // Create an empty vector and fill it
     let mut fruits = Vec::new();
     fruits.push("apple");
     fruits.push("tomato");
     fruits.push("pear");
-    println!("Fruits: {:?}", fruits);
+    println!("fruits: {:?}", fruits);
+
+    // Initialize the vector with a value
+    // Here, we fill our vector with five zeroes
+    let bunch_of_zeroes = vec![0; 5];
+    println!("bunch_of_zeroes: {:?}", bunch_of_zeroes);
+
+    
 }
