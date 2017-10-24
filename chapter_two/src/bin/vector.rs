3b30866e6b034933c1715db88c0feb7c05ed2cf3
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-10-24 11:09:50 +0200

    Add basic vector creation

--- chapter_two/src/bin/vector.rs
+++ chapter_two/src/bin/vector.rs
@@ -1,3 +1,14 @@
 fn main() {
-    // Todo: Add stuff
+    // Create a vector with some elements
+    let fruits = vec!["apple", "tomato", "pear"];
+    // A vector cannot be directly printed
+    // But we can debug-print it
+    println!("Fruits: {:?}", fruits);
+
+    // Create an empty vector and fill it
+    let mut fruits = Vec::new();
+    fruits.push("apple");
+    fruits.push("tomato");
+    fruits.push("pear");
+    println!("Fruits: {:?}", fruits);
 }
