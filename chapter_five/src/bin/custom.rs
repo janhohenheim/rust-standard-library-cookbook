ca0ed2f7089449b80c26b197647faf7410103fbc
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-01-05 11:58:15 +0100

    Add custom derive example

---
+++ chapter_five/src/bin/custom.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate custom_derive;
+
+// trait definitions have to be in "consumer" crate
+trait HelloWorld {
+    // This method will send a friendly greeting
+    fn hello_world();
+}
+
+// thanks to the code in the custom_derive crate
+// we can derive from HelloWorld in order to provide
+// an automatic implementation for the HelloWorld trait
+#[derive(HelloWorld)]
+struct Switzerland;
+
+#[derive(HelloWorld)]
+struct Britain;
+
+#[derive(HelloWorld)]
+// We can use an optional attribute to change the message
+#[HelloWorldName = "the Land Down Under"]
+struct Australia;
+
+
+fn main() {
+    Switzerland::hello_world();
+    Britain::hello_world();
+    Australia::hello_world();
+}
