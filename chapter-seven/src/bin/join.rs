5865eff2b08dfb5fd754fb6b72cd222018f71f1f
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-02-15 22:46:17 +0100

    Add recursive call to join example

--- chapter-seven/src/bin/join.rs
+++ chapter-seven/src/bin/join.rs
@@ -20,8 +20,23 @@ fn main() {
         height: 30,
         width: 20,
     };
+    // rayon::join makes closures run potentially in parallel and
+    // returns their returned values in a tuple
     let (area, perimeter) = rayon::join(|| rect.area(), || rect.perimeter());
     println!("{:?}", rect);
     println!("area: {}", area);
     println!("perimeter: {}", perimeter);
+
+    let fib = fibonacci(6);
+    println!("The sixth number in the fibonacci sequence is {}", fib);
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 || n == 1 {
+        n
+    } else {
+        // rayon::join can really shine in recursive functions
+        let (a, b) = rayon::join(|| fibonacci(n - 1), || fibonacci(n - 2));
+        a + b
+    }
 }
