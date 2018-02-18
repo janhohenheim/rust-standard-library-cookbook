1d55ce295af9e58e82f17d5ae5339c34e9b9b1ac
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-02-18 18:45:23 +0100

    Start atomic recipe with custom mutex implementation

--- chapter-seven/src/bin/atomic.rs
+++ chapter-seven/src/bin/atomic.rs
@@ -1,55 +1,77 @@
 use std::sync::Arc;
-use std::sync::atomic::{fence, AtomicBool, AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering, ATOMIC_BOOL_INIT, ATOMIC_USIZE_INIT};
 use std::thread;
+use std::ops::{Deref, DerefMut};
+use std::cell::UnsafeCell;
 
-static GLOBAL_THREAD_COUNT: AtomicUsize = ATOMIC_USIZE_INIT;
-
-fn main() {
-    let old_thread_count = GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
-    println!("live threads: {}", old_thread_count + 1);
+// NaiveMutex is an easy, albeit very suboptimal,
+// implementation of a Mutex, similar to std::sync::Mutex
+pub struct NaiveMutex<T> {
+    locked: AtomicBool,
+    data: UnsafeCell<T>,
 }
 
-fn spinlock() {
-    let spinlock = Arc::new(AtomicUsize::new(1));
-
-    let spinlock_clone = spinlock.clone();
-    let thread = thread::spawn(move || {
-        spinlock_clone.store(0, Ordering::SeqCst);
-    });
-
-    // Wait for the other thread to release the lock
-    while spinlock.load(Ordering::SeqCst) != 0 {
-        println!("Waiting for the other thread to set the counter to zero");
-    }
-
-    if let Err(panic) = thread.join() {
-        println!("Thread had an error: {:?}", panic);
-    }
-
-    use std::sync::atomic::{AtomicUsize, Ordering};
+pub struct NaiveMutexGuard<'a, T: 'a> {
+    naive_mutex: &'a NaiveMutex<T>,
 }
 
-// A mutual exclusion primitive based on spinlock.
-pub struct Mutex {
-    flag: AtomicBool,
-}
-
-impl Mutex {
-    pub fn new() -> Mutex {
-        Mutex {
-            flag: AtomicBool::new(false),
+impl<T> NaiveMutex<T> {
+    pub fn new(data: T) -> Self {
+        NaiveMutex {
+            locked: ATOMIC_BOOL_INIT,
+            data: UnsafeCell::new(data),
         }
     }
 
-    pub fn lock(&self) {
-        while !self.flag.compare_and_swap(false, true, Ordering::Relaxed) {}
-        // This fence synchronizes-with store in `unlock`.
-        fence(Ordering::Acquire);
-    }
-
-    pub fn unlock(&self) {
-        self.flag.store(false, Ordering::Release);
+    pub fn lock(&self) -> NaiveMutexGuard<T> {
+        // The following algorithm is called a "spinlock", because it keeps
+        // the current thread blocked by doing nothing (it keeps it "spinning")
+        while self.locked.compare_and_swap(false, true, Ordering::SeqCst) {}
+        NaiveMutexGuard { naive_mutex: self }
     }
 }
 
-// To do maybe implement Peterson lock
+unsafe impl<T: Send> Sync for NaiveMutex<T> {}
+
+// Automatically unlock the underlying resource on drop
+impl<'a, T> Drop for NaiveMutexGuard<'a, T> {
+    fn drop(&mut self) {
+        self.naive_mutex.locked.store(false, Ordering::SeqCst);
+    }
+}
+
+impl<'a, T> Deref for NaiveMutexGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &mut *self.naive_mutex.data.get() }
+    }
+}
+
+impl<'a, T> DerefMut for NaiveMutexGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.naive_mutex.data.get() }
+    }
+}
+
+unsafe impl<'a, T: Sync> Sync for NaiveMutexGuard<'a, T> {}
+
+fn main() {
+    let naive_mutex = Arc::new(NaiveMutex::new(1));
+    let updater = {
+        let naive_mutex = naive_mutex.clone();
+        thread::spawn(move || {
+            let mut val = naive_mutex.lock();
+            *val = 2;
+        })
+    };
+    let printer = {
+        let naive_mutex = naive_mutex.clone();
+        thread::spawn(move || {
+            let val = naive_mutex.lock();
+            println!("The value in the naive mutex is: {}", *val);
+        })
+    };
+    updater.join().expect("The updater thread panicked");
+    printer.join().expect("The printer thread panicked");
+}
