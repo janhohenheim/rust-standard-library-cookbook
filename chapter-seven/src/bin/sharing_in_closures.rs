68218fde150073a31622caa4612188fbbf331ec5
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-02-18 20:27:58 +0100

    Write sharing recipe without comments

--- chapter-seven/src/bin/sharing_in_closures.rs
+++ chapter-seven/src/bin/sharing_in_closures.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use std::thread;
+use std::sync::Arc;
+
+fn main() {
+    let some_resource = Arc::new("Hello World".to_string());
+    let thread_a = {
+        let some_resource = some_resource.clone();
+        thread::spawn(move || {
+            println!("Thread A says: {}", some_resource);
+        })
+    };
+    let thread_b = {
+        let some_resource = some_resource.clone();
+        thread::spawn(move || {
+            println!("Thread B says: {}", some_resource);
+        })
+    };
+
+    thread_a.join().expect("Thread A panicked");
+    thread_b.join().expect("Thread B panicked");
+}
