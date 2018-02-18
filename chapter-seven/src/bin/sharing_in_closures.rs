cc08337f4e21062623214ed828eb99f3a3334faa
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-02-18 20:37:53 +0100

    Add comments to the arc recipe

--- chapter-seven/src/bin/sharing_in_closures.rs
+++ chapter-seven/src/bin/sharing_in_closures.rs
@@ -2,9 +2,15 @@ use std::thread;
 use std::sync::Arc;
 
 fn main() {
+    // An Arc ("Atomically Reference Counted") is used the exact
+    // same way as an Rc, but also work in a parallel context
     let some_resource = Arc::new("Hello World".to_string());
+
+    // We use it to give a new thread ownership of a clone of the Arc
     let thread_a = {
+        // It is very common to give the clone the same name as the original
         let some_resource = some_resource.clone();
+        // The clone is then moved into the closure:
         thread::spawn(move || {
             println!("Thread A says: {}", some_resource);
         })
@@ -16,6 +22,7 @@ fn main() {
         })
     };
 
+    // .join() blocks the main thread until the other thread is done
     thread_a.join().expect("Thread A panicked");
     thread_b.join().expect("Thread B panicked");
 }
