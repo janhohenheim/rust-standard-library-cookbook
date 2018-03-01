d7feb5afcdbb394062e30fb148e814d11fd2b236
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-03-01 11:15:04 +0100

    Add custom generator -> iterator wrapper

--- chapter-ten/src/bin/generator.rs
+++ chapter-ten/src/bin/generator.rs
@@ -1,5 +1,19 @@
 #![feature(generators, generator_trait, conservative_impl_trait)]
+
 use std::ops::{Generator, GeneratorState};
+struct GeneratorIterator<G>(G);
+impl<G> Iterator for GeneratorIterator<G>
+where
+    G: Generator<Return = ()>,
+{
+    type Item = G::Yield;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.0.resume() {
+            GeneratorState::Yielded(value) => Some(value),
+            GeneratorState::Complete(_) => None,
+        }
+    }
+}
 
 fn main() {
     let mut generator = || {
@@ -15,13 +29,12 @@ fn main() {
         GeneratorState::Complete("foo") => {}
         _ => panic!("unexpected value from resume"),
     }
-
     let fib: Vec<_> = fibonacci().take(10).collect();
     println!("First 10 numbers of the fibonacci sequence: {:?}", fib);
 }
 
 fn fibonacci() -> impl Iterator<Item = u32> {
-    || {
+    GeneratorIterator(move || {
         let mut curr = 0;
         let mut next = 1;
         loop {
@@ -30,5 +43,5 @@ fn fibonacci() -> impl Iterator<Item = u32> {
             curr = next;
             next += old;
         }
-    }
+    })
 }
