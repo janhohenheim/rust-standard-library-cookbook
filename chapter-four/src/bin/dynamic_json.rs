772a8f34263ee81ce83b77e14ef5b56d33966ed3
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-03-04 22:58:25 +0100

    Make input more native to system

--- chapter-four/src/bin/dynamic_json.rs
+++ chapter-four/src/bin/dynamic_json.rs
@@ -8,13 +8,9 @@ fn main() {
     // A HashMap is the same as a JSON without any schema
     let mut key_value_map = HashMap::new();
     let stdin = io::stdin();
-    println!("Please enter a key and a value or cancel with 'stop'");
+    println!("Enter a key and a value");
     for input in stdin.lock().lines() {
         let input = input.expect("Failed to read line");
-        if input == "stop" {
-            break;
-        }
-
         let key_value: Vec<_> = input.split_whitespace().collect();
         let key = key_value[0].to_string();
         let value = key_value[1].to_string();
@@ -22,7 +18,10 @@ fn main() {
         println!("Saving key-value pair: {} -> {}", key, value);
         // The json! macro lets us convert a value into its JSON representation
         key_value_map.insert(key, json!(value));
-        println!("Enter another one or cancel with 'stop'");
+        println!(
+            "Enter another pair or stop by pressing '{}'",
+            END_OF_TRANSMISSION
+        );
     }
     // to_string_pretty returns a JSON with nicely readable whitespace
     let json =
@@ -30,3 +29,9 @@ fn main() {
     println!("Your input has been made into the following JSON:");
     println!("{}", json);
 }
+
+#[cfg(target_os = "windows")]
+const END_OF_TRANSMISSION: &str = "Ctrl Z";
+
+#[cfg(not(target_os = "windows"))]
+const END_OF_TRANSMISSION: &str = "Ctrl D";
