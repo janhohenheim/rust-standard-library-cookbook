ef61bdf4bfda6fc32cbacc4a55312d6aa6da2bd7
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-03-08 15:15:10 +0100

    Add echoing example

--- chapter-eight/src/bin/echo_server.rs
+++ chapter-eight/src/bin/echo_server.rs
@@ -1,23 +1,14 @@
-extern crate futures;
 extern crate hyper;
 
-use futures::future::Future;
-
 use hyper::{Method, StatusCode};
-use hyper::server::{Http, Request, Response, Service};
+use hyper::server::{Http, Request, Response};
 
-struct Echo;
-impl Service for Echo {
-    // boilerplate hooking up hyper's server types
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    // The future representing the eventual Response your call will
-    // resolve to. This can change to whatever Future you need.
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+use hyper::server::{const_service, service_fn};
 
-    fn call(&self, req: Request) -> Self::Future {
-        let mut response = Response::new();
+fn main() {
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let echo = const_service(service_fn(|req: Request| {
+        let mut response: Response<hyper::Body> = Response::new();
 
         match (req.method(), req.path()) {
             (&Method::Get, "/") => {
@@ -31,12 +22,8 @@ impl Service for Echo {
             }
         };
 
-        Box::new(futures::future::ok(response))
-    }
-}
-
-fn main() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
+        Ok(response)
+    }));
+    let server = Http::new().bind(&addr, echo).unwrap();
     server.run().unwrap();
 }
