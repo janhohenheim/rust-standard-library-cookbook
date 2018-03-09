8eff4cd44ffd67d7fbc6c5caea1c9c71d054177a
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-03-09 01:16:22 +0100

    Reuse response

--- chapter-nine/src/bin/echo_server.rs
+++ chapter-nine/src/bin/echo_server.rs
@@ -3,22 +3,37 @@ extern crate hyper;
 use hyper::{Method, StatusCode};
 use hyper::server::{const_service, service_fn, Http, Request, Response};
 use hyper::header::{ContentLength, ContentType};
-
+use std::net::SocketAddr;
 fn main() {
     let addr = "[::1]:3000".parse().expect("Failed to parse address");
+    run_echo_server(&addr).expect("Failed to run webserver");
+}
+
+fn run_echo_server(addr: &SocketAddr) -> Result<(), hyper::Error> {
     let echo = const_service(service_fn(|req: Request| {
-        Ok(match (req.method(), req.path()) {
+        let mut response: Response<hyper::Body> = Response::new();
+
+        match (req.method(), req.path()) {
             (&Method::Get, "/") => {
                 const ERR_MSG: &str = "Try doing a POST at /echo";
-                Response::<hyper::Body>::new()
-                    .with_header(ContentType::plaintext())
-                    .with_header(ContentLength(ERR_MSG.len() as u64))
-                    .with_body(ERR_MSG)
+                {
+                    let headers = response.headers_mut();
+                    headers.set(ContentType::plaintext());
+                    headers.set(ContentLength(ERR_MSG.len() as u64));
+                }
+                response.set_body(ERR_MSG);
             }
-            (&Method::Post, "/echo") => Response::<hyper::Body>::new().with_body(req.body()),
-            _ => Response::<hyper::Body>::new().with_status(StatusCode::NotFound),
-        })
+            (&Method::Post, "/echo") => {
+                response.set_body(req.body());
+            }
+            _ => {
+                response.set_status(StatusCode::NotFound);
+            }
+        };
+
+        Ok(response)
     }));
-    let server = Http::new().bind(&addr, echo).unwrap();
-    server.run().unwrap();
+
+    let server = Http::new().bind(&addr, echo)?;
+    server.run()
 }
