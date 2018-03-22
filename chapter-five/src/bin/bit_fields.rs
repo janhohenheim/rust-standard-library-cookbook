a6ab363801c7d2e19f84afdf2c10bef9e4914cee
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-03-22 21:47:34 +0100

    Fix indentation

--- chapter-five/src/bin/bit_fields.rs
+++ chapter-five/src/bin/bit_fields.rs
@@ -5,7 +5,7 @@ bitflags! {
     struct Spices: u32 {
         const SALT       = 0b0000_0001;
         const PEPPER     = 0b0000_0010;
-        const CHILI     = 0b0000_0100;
+        const CHILI      = 0b0000_0100;
         const SAFFRON    = 0b0000_1000;
         const ALL        = Self::SALT.bits
                          | Self::PEPPER.bits
